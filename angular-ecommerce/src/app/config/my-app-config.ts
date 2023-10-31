62bb82882d4b23a6347545f1fb26a4dd7474817a
jd <jhojan_54_56@hotmail.com>
2023-10-31 15:47:15 -0500

    another4configuration

---
+++ angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oac9ukczc5VKhOO15d7',
+        issuer: 'https://dev-52724161.okta.com/oauth2/default',
+        redirectUri: 'https://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
