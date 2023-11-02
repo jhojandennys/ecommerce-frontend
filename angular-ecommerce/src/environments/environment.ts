4b603243cf97b2e850739323868a09c20ba834cf
jd <jhojan_54_56@hotmail.com>
2023-11-02 15:12:47 -0500

    enviroment stripe conf

---
+++ angular-ecommerce/src/environments/environment.ts
@@ -0,0 +1,18 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  shopApiUrl: "https://localhost:8443/api",
+  stripePublishableKey: "pk_test_51NfcrTBG4gmNY8OfUMP3PnZdXi0ldriA1DjGrpyKYK0INo8GzvtQxHvBoyoYtM4B5HlSqFIIK7z5BOZv21GMQPbw006FXIBMcv"
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
