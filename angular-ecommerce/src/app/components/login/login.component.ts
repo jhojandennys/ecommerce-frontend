c470d56d184a85af6409c22af7c5720f5daf6260
jd <jhojan_54_56@hotmail.com>
2023-11-02 09:24:21 -0500

    Okta signIn

---
+++ angular-ecommerce/src/app/components/login/login.component.ts
@@ -0,0 +1,59 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { OKTA_AUTH } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
+import OktaSignIn from '@okta/okta-signin-widget';
+
+import myAppConfig from '../../config/my-app-config';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  oktaSignin: any;
+  oktaSignUp: any;
+
+  constructor(@Inject(OKTA_AUTH) private oktaAuth: OktaAuth) {
+
+    this.oktaSignin = new OktaSignIn({
+      logo: 'assets/images/logo.png',
+      baseUrl: myAppConfig.oidc.issuer.split('/oauth2')[0],
+    registration: {
+       
+      }, features: {
+        // Used to enable registration feature on the widget.
+        // https://github.com/okta/okta-signin-widget#feature-flags
+         registration: true // REQUIRED
+      },
+      redirectUri: myAppConfig.oidc.redirectUri,
+      authParams: {
+        pkce: true,
+        issuer: myAppConfig.oidc.issuer,
+        scopes: myAppConfig.oidc.scopes
+      }
+    });
+
+   }
+   signIn() {
+    this.oktaAuth.signInWithRedirect(); // Trigger sign-in when the button is clicked
+  }
+  
+  ngOnInit(): void {
+    this.oktaSignin.remove();
+
+    this.oktaSignin.renderEl({
+      el: '#okta-sign-in-widget'}, // this name should be same as div tag id in login.component.html
+      (response: any) => {
+        if (response.status === 'SUCCESS') {
+          this.oktaAuth.signInWithRedirect();
+        }
+      },
+      (error: any) => {
+        throw error;
+      }
+    );
+  }
+
+}
